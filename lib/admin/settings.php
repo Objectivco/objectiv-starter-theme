03ad31ae224782bbdd0e499a9e73274f34536152
Wesley Cole <wes@cgd.io>
2016-12-17 19:31:16 -0500

    adding theme settings footer

--- lib/admin/settings.php
+++ lib/admin/settings.php
@@ -18,10 +18,6 @@ class ObjectivSettings extends ObjectivAdmin {
 			'obj_theme_settings_admin_options',
 			array(
 				'main_menu' => array(
-					'sep' => array(
-						'sep_position'   => '58.995',
-						'sep_capability' => 'edit_theme_options',
-					),
 					'page_title' => 'Theme Settings',
 					'menu_title' => 'Objectiv',
 					'capability' => 'edit_theme_options',
@@ -73,6 +69,20 @@ class ObjectivSettings extends ObjectivAdmin {
                     echo 'analytics';
                 }
                 ?>
+            </div>
+        </div>
+        <div class="obj-footer">
+            <div class="obj-footer__container">
+                <div class="obj-footer__image">
+                    <img src="<?php echo PARENT_THEME_ADMIN_IMAGES_DIR . '2016_headshots_Erik.jpg'; ?>" />
+                </div>
+                <div class="obj-footer__content">
+                    <h4 class="obj-footer__header">Need Help? We are one email away.</h4>
+                    <p class="obj-footer__description">We won't ever leave you in the lurch. If you have a question or found a bug with your website, please let us know.</p>
+                    <p class="obj-footer__description">
+                        <a href="http://objectiv.co/contact" title="Go to Objectiv Support" class="button button-primary">Contact Us</a>
+                    </p>
+                </div>
             </div>
         </div>
         <?php
