ad91ca2c0b4083a3768b76b140df8584e580657c
Wes Cole <wesley.cole87@gmail.com>
2017-06-16 20:37:53 -0400

    removing the uneeded functions

--- functions.php
+++ functions.php
@@ -73,13 +73,6 @@ class ObjectivSite extends TimberSite
 
     function __construct()
     {
-
-        // Add admin body class
-        add_filter( 'admin_body_class', array( $this, 'obj_admin_body_class' ) );
-        // Enqueue admin assets
-        add_action( 'admin_enqueue_scripts', array( $this, 'obj_admin_assets' ) );
-        // Remove admin pages for non admin users
-        add_action( 'admin_init', array( $this, 'obj_remove_admin_menus' ) );
         // Add theme support for menus
         add_theme_support( 'menus' );
         // Add theme support for post_thumbnails
@@ -118,54 +111,6 @@ class ObjectivSite extends TimberSite
     }
 
     /**
-     * Admin Body Class
-     *
-     * @param  array $classes
-     *
-     * @since 1.0
-     */
-    function obj_admin_body_class($classes)
-    {
-        $screen = get_current_screen();
-
-        if ('toplevel_page_objectiv' == $screen->base) {
-            $classes .= ' ' . 'obj-admin';
-        }
-        return $classes;
-    }
-
-    /**
-     * Load Admin Assets
-     *
-     * @since 1.0
-     */
-    function obj_admin_assets()
-    {
-        if (is_admin()) {
-            wp_enqueue_style(
-                'obj_admin_css',
-                PARENT_THEME_URI .
-                'admin_assets/sass/admin.css',
-                array(),
-                PARENT_THEME_VERSION
-            );
-        }
-    }
-
-    /**
-     * Register Sidebars
-     *
-     * @since 1.0
-     */
-    function obj_widgets_init()
-    {
-        register_sidebar( array(
-            'name'          => __( 'Primary Sidebar', 'objectiv' ),
-            'id'            => 'primary-sidebar'
-        ) );
-    }
-
-    /**
      * Add variables to site context
      * @param object $context
      *
@@ -189,6 +134,19 @@ class ObjectivSite extends TimberSite
     function obj_add_to_twig($twig)
     {
         return $twig;
+    }
+
+    /**
+     * Register Sidebars
+     *
+     * @since 1.0
+     */
+    function obj_widgets_init()
+    {
+        register_sidebar( array(
+            'name'          => __( 'Primary Sidebar', 'objectiv' ),
+            'id'            => 'primary-sidebar'
+        ) );
     }
 
     /**
