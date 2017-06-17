de7e04c82a9897ae9a94ce8b58f9adadfbb72072
Wes Cole <wesley.cole87@gmail.com>
2017-06-16 20:46:53 -0400

    small tweaks and removing uneeded template files

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
 $context = Timber::get_context();
 $context['posts'] = Timber::get_posts();
-$templates = array( 'index.twig' );
+$templates = array( 'index.twig', 'home.twig', 'front-page.twig' );
 
 if (is_home()) {
     array_unshift( $templates, 'home.twig' );
